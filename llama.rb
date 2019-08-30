d661d4c1bc60464291a4542980430aff745efcdc
Rory <rory_bell_88@hotmail.com>
2019-08-30 15:51:36 +1000

    made a good start on the llama warrior class and sub classes. Testing done to ensure the additional methods wihtin subclasses are working and all looking good.

--- llama.rb
+++ llama.rb
@@ -1,17 +1,6 @@
-module PlusAttack
-    @attack += 1
-end
-
-module PlusHp
-    @health +=3
-end
- module PLusDef
-   @defense +=1
-
-module
 #Super class Llamma warriors. Instance variables define the health,attack & defense stats for all llamas within the initialize method.
-Class LlamaWarriors
-attr_accessor
+class LlamaWarriors
+attr_accessor :health, :attack, :defense, :guspatcho_defense, :karl_attack, :lemonywinks_health
 
   def initialize
     @health = 10
@@ -20,12 +9,43 @@ attr_accessor
   end 
 
 end
+# Sub classes of The llama warrior class defined below for inheritance 
 
-class Karl < LlamaWarriors
+class Karl < LlamaWarriors # Karl has higher attack than the other llamas, value returned to @karl_attack
+  def k_able
+    @karl_attack = @attack + 1
+    p "Hey I'm Karl...I like to wear funny hats.... I am pleasant and speak calmly. Dont be fooled though!"
+    p "Im secretly a phycopath that get's the rumblies that only Sloth hands can satisfy."
+      "Check out my Stats! Health: #{@health} Attack: #{@karl_attack} Defense: #{@defense}"
+  end
+end 
+
+class Guspatcho < LlamaWarriors # Guspatcho as better defense than the other llamas, value retuned to @Guspatcho_defense
+  def g_able
+    @guspatcho_defense = @defense + 1
+    p "I am a the Guspatcho a!...I a come from the hills in Italia!. This is where i learna to defenda my territory!"
+    p "Fighting off sloths Pffft..easy peasy! You willa see that my a statsa reflect my skills!"
+      "Check out mya defensa! Health: #{@health} Attack: #{@attack} Defense: #{@guspatcho_defense}"
+  end
 end
 
-class Guspatcho < LlamaWarriors
+class LemonyWinks < LlamaWarriors #Lemony winks has higher health than other llamas, value returned to @lemony_health
+  def l_able
+    @lemony_health = @health += 3
+    p "Namaste! I'm Lemony-winks....I am a picture of llama vitality and health."
+    p "My passtimes include llama yoga & long walks On the beach "
+    p "As a result of the care that i take for myself im really hard to kill!"
+      "Check the Stats! Health: #{@lemony_health} Attack: #{@attack} Defense: #{@defense}"
+  end
 end
 
-class LemonyWinks < LlamaWarriors
 
+karl = Karl.new
+guspatcho = Guspatcho.new
+lemonywinks = LemonyWinks.new
+
+p karl.k_able
+p""
+p guspatcho.g_able
+p""
+p lemonywinks.l_able
