73d81fec6400c52d705e25e947dc2137268b4715
Rory <rory_bell_88@hotmail.com>
2019-08-30 14:49:52 +1000

    Made a start set up basic class structure with modules

--- llama.rb
+++ llama.rb
@@ -1,2 +1,31 @@
-Class Llama_warriors
+module PlusAttack
+    @attack += 1
 end
+
+module PlusHp
+    @health +=3
+end
+ module PLusDef
+   @defense +=1
+
+module
+#Super class Llamma warriors. Instance variables define the health,attack & defense stats for all llamas within the initialize method.
+Class LlamaWarriors
+attr_accessor
+
+  def initialize
+    @health = 10
+    @attack = 3
+    @defense = 2
+  end 
+
+end
+
+class Karl < LlamaWarriors
+end
+
+class Guspatcho < LlamaWarriors
+end
+
+class LemonyWinks < LlamaWarriors
+
