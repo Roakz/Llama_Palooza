352df9aab6465dfa01819cb40d056279393aae73
Rory <rory_bell_88@hotmail.com>
2019-09-05 10:54:12 +1000

    added a couple of tests for the llama class

--- tests/llama_class_tests.rb
+++
@@ -1,9 +0,0 @@
-require "test-unit"
-require_relative "../llama_classes"
-
-class llama_class_tests < Test::Unit::TestCase
-
-
-    
-end    
-end
---
+++ tests/llama_test.rb
@@ -0,0 +1,24 @@
+require "test/unit"
+require_relative "../llama_classes"
+require 'artii'
+
+class LlamaTest < Test::Unit::TestCase
+
+    #Testing print artii method
+    def test_print_artii
+        @artii = LlamaWarriors.new
+        @var = Artii::Base.new :font => 'slant'
+        @var = @var.asciify("Llama")
+        assert_equal(@var, @artii.print_artii("Llama") )    
+    end
+    # Testing to make sure that a subclass generated Llama displays the correct atts
+        def test_llama_atts
+        @karl = Karl.new
+        assert_equal(@karl.llama_atts[:llama_a], 6)
+        @gus = Guspatcho.new
+        assert_equal(@gus.llama_atts[:llama_d], 3)
+        @lem = LemonyWinks.new
+        assert_equal(@lem.llama_atts[:llama_h], 13)
+    end
+
+end
