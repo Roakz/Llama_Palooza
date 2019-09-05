64c4b3d785ab3ba3fe5bfbbcaec07174eea51218
Rory <rory_bell_88@hotmail.com>
2019-09-05 16:45:26 +1000

    added testing document for a log of overall app tests to be completed

--- tests/sloth_tests.rb
+++ tests/sloth_tests.rb
@@ -8,18 +8,18 @@ sloth2 = Sloth.new
 sloth3 = Sloth.new
 
 
-puts sloth.com_health
-puts sloth.com_attack
-puts sloth.com_defense
+puts sloth.comp_health
+puts sloth.comp_attack
+puts sloth.comp_defense
 
-puts sloth1.com_health
-puts sloth1.com_attack
-puts sloth1.com_defense
+puts sloth1.comp_health
+puts sloth1.comp_attack
+puts sloth1.comp_defense
 
-puts sloth2.com_health
-puts sloth2.com_attack
-puts sloth2.com_defense
+puts sloth2.comp_health
+puts sloth2.comp_attack
+puts sloth2.comp_defense
 
-puts sloth3.com_health
-puts sloth3.com_attack
-puts sloth3.com_defense
+puts sloth3.comp_health
+puts sloth3.comp_attack
+puts sloth3.comp_defense
